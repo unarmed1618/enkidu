a7980ac3a6a1953a32afaf182ee5a8c6fc6ac750
Kalia Jade Darrow <darrowj08@students.ecu.edu>
2015-08-18 20:04:28 -0400

    Added files

---
+++ src/enkidu/main/Enkidu.java
@@ -0,0 +1,13 @@
+package enkidu.main;
+
+public class Enkidu {
+
+	public static void main(String[] args) {
+		System.out.println("Attempting to start Enkidu from console...");
+		System.out.println("Activating Enkidu");
+		
+		
+	}
+	
+	
+}
